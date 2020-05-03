2592c64dbac8471c3c960643602fe0760dd77460
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-05-03 20:47:33 +0900

    読み込みファイル変更

--- template/tmp.header.php
+++ template/tmp.header.php
@@ -6,12 +6,11 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <nav class="navbar sticky-top navbar-expand-lg navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="#" style="font-size:25px; font-style:italic;">Programming Word Search</a>
+    <nav class="navbar sticky-top navbar-expand-lg navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php" style="font-size:25px; font-style:italic;">Programming Word Search</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
@@ -26,14 +25,12 @@
                         </div>
                     </li>
                 </ul>
-
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search by title" aria-label="Search">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
         </nav>
-
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
