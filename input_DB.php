245da6b4a3e94fcea7ebb862a162faca7aae39f1
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-05-04 13:52:22 +0900

    入力結果の表示

--- input_DB.php
+++ input_DB.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <title>word search</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="stylesheet2.css">
     </head>
     <body>
         <?php 
@@ -13,6 +14,21 @@
             $input_DB = $PDO->prepare("INSERT INTO words SET title=?, body=?, category=?, created_at=NOW()");
             $input_DB-> execute(array($_POST["title"], $_POST["body"], $_POST["category"]));
         ?>
-        
+
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2 style="color:lightgreen;">-- Created!! --</h2>
+                        <h4>タイトル</h4>
+                        <p><?php echo $_POST["title"];?></p>
+                        <h4>内容</h4>
+                        <p><?php echo $_POST["body"];?></p>
+                        <h4>カテゴリ</h4>
+                        <p><?php echo $_POST["category"];?></p>
+                        <a class="btn btn-outline-success my-2 my-sm-0" href="index.php" role="button">一覧に戻る</a>
+                    </div>
+                </div>
+            </div>
+                    
     </body>
 </html>
