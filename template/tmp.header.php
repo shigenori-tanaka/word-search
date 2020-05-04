f71a5454b28c0f16f3d50b26b9335b4f3df5a2c7
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-05-04 21:19:00 +0900

    actionを追加

--- template/tmp.header.php
+++ template/tmp.header.php
@@ -18,8 +18,8 @@
                 </div>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search by title" aria-label="Search">
+        <form class="form-inline my-2 my-lg-0" action="./search.php">
+            <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search by title" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
