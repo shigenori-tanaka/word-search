d1c1bf7560645dbc51216dc3c85bf0c40453e2c2
tanaka-shigenori <s.tanaka0917@gmail.com>
2020-05-07 14:07:12 +0900

    検索結果を作成

--- searchResult/HTML&CSS.php
+++ searchResult/HTML&CSS.php
@@ -10,8 +10,28 @@
         <?php 
             require_once("../DBconnect.php");
             require_once("tmp.headerResult.php");
-            echo "hello";
         ?>
+        <!-- category "HTML&CSS"を取得 -->
+        <?php $htmlDB = $PDO->query("SELECT * FROM words WHERE category='HTML&CSS' "); ?>
+        <!-- ここまで category "HTML&CSS"を取得 -->
+        <article style="width:95%; margin:0 auto; ">
+            <?php while($html = $htmlDB->fetch()): ?>
+                <p style="word-wrap:break-word;"> <?php echo htmlspecialchars($html["title"]); ?> </p>
+                <p style="word-wrap:break-word; white-space: pre-wrap;" > <?php echo htmlspecialchars($html["body"]); ?> </p>
+                <p> <?php echo $html["category"]; ?> </p>
+                <a class="btn btn-outline-success btn-sm" href="../edit.php?id=<?php echo $html["id"] ?>" role="button" style="margin-right: 10px;">編集</a>
+                <form action="../delete.php?id=<?php echo $html["id"] ?>" onsubmit="return confirm_test()" method="POST" style="display:inline-block;">
+                    <button class="btn btn-outline-secondary btn-sm" type="submit">削除</button>
+                </form>
+                <hr>
+            <?php endwhile ?>
+        </article>
 
+        <script>
+            function confirm_test() {
+            var select = confirm("本当に削除しますか？\n「OK」で削除\n「キャンセル」で中止");
+            return select;
+            }
+        </script>
     </body>
 </html>
